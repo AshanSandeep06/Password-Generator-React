011c4b1d3cf12fa5f8a558ad863fe5adedfa1c27
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-30 14:23:08 +0530

    Added Material UI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,17 @@ export default function App() {
     <section className="w-screen h-screen">
       <form className="w-full h-full py-8 px-32 flex flex-col gap-3 items-center justify-center">
         <div className="pt-5 w-1/2 px-16 flex flex-col gap-4 items-center shadow-xl drop-shadow-md h-1/2 rounded-xl border border-slate-100">
-          
+          <div>
+            <h1>Automatic Password Generator</h1>
+          </div>
+
+          <div>
+            <input type="text" />
+          </div>
+
+          <div>
+            <button>Generate Password</button>
+          </div>
         </div>
       </form>
     </section>
