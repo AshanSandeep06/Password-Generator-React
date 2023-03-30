d569e438aee7ba9d1dea85de17dbbd5a7c321f00
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-30 19:49:24 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,14 @@ export default function App() {
           </div>
 
           <div className="flex gap-8 justify-center w-full">
-            <button
+            <button id="btnGeneratePassword"
               type="button"
               className="mt-2 bg-blue-900 text-white py-[10px] px-[18px] rounded-lg font-medium"
             >
               Generate Password
             </button>
 
-            <button
+            <button id="btnClear"
               type="button"
               className="mt-2 border border-yellow-500 text-yellow-500 py-[10px] px-[18px] rounded-lg font-medium hover:bg-yellow-500 hover:text-white"
             >
