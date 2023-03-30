19244f7ce15a2ea591eed3956d8421aa4f30df71
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-30 14:11:41 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import "./App.css";
 
 export default function App() {
   return (
-    <section className="h-full">
+    <section className="w-screen h-screen">
       <form className="w-full h-full py-8 px-32 flex flex-col gap-3 items-center justify-center">
-        <div className="pt-5 w-1/2 px-16 flex flex-col gap-4 items-center shadow-xl h-[300px] rounded-xl">
+        <div className="pt-5 w-1/2 px-16 flex flex-col gap-4 items-center shadow-xl h-1/2 rounded-xl">
           <h1>Login Form</h1>
         </div>
       </form>
