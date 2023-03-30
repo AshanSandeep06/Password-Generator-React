135edc8cdec13562d463db858c2b3927f963ad6c
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-30 15:28:11 +0530

    Content Updated in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     <section className="w-screen h-screen font-poppins font-semibold">
       <form className="w-full h-full py-8 px-32 flex flex-col gap-3 items-center justify-center">
         <div className="pt-5 w-1/2 px-16 flex flex-col gap-4 items-center shadow-xl drop-shadow-md h-1/2 rounded-xl border border-slate-100">
-          <div className="mb-3">
+          <div className="mb-4 mt-3">
             <h1 className="text-[25px]">Automatic Password Generator</h1>
           </div>
 
