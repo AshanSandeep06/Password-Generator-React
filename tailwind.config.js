4846ad11e261fb2f2a8a2e2e24edaeff08aa9439
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-30 13:38:01 +0530

    Added custom color to the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bg-blue-custom":{
+          100: '##16168b'
+        }
+      }
+    },
   },
   plugins: [],
 }
